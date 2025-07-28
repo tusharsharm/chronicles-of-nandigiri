6910d0edf5d9aa99897d51d82651bad0470fdfd3
Tushar Sharma <124043637+tusharsharm@users.noreply.github.com>
2025-07-28 15:55:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -448,7 +448,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Among Us Clone server running on port ${PORT}`);
 });
